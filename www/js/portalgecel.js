c5e4bcd7e87e2ccafff5797a3d08daa6037abad1
valdirviana <valdir_151@hotmail.com>
2014-04-08 02:13:11 -0300

    retirar alert retorno

--- www/js/portalgecel.js
+++ www/js/portalgecel.js
@@ -174,7 +174,7 @@ function Sair() {
                     success: function (data) {
 
                         var retorno = $(data).find('int').text();
-                        alert(retorno);
+                       
 
                         if (retorno < 1) {
 
